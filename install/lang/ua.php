83b132982c6ac2bc0038b673220905f4a8bf8e67
Basil McCormac <basilmccormac@aol.com>
2013-07-15 12:28:13 +0300

    UA language converted to UTF-8 encoding

---
+++ install/lang/ua.php
@@ -0,0 +1,92 @@
+<?php
+
+	$lang['lang_name']='Українська';
+
+	$lang['installation']='Встановлення';
+	$lang['cant_connect_to_database']='Не можу під\'єднатися до бази даних';
+	$lang['cant_create_directory']='Не можу створити теку';
+	$lang['cant_create_table']='Не можу створити таблицю';
+
+	$lang['next_step']='Наступний крок';
+	$lang['finish']='Завершити встановлення';
+	$lang['OK']='Гаразд';
+	$lang['warning']='Увага';
+	$lang['error']='Помилка';
+	$lang['fatal_error']='Критична помилка';
+	$lang['directory_exist']='Тека вже існує';
+
+	$lang['language_set']='Встановлення мови';
+	$lang['directory_creation']='Створення теки';
+	$lang['table_creation']='Створення таблиці';
+	$lang['data_post_into_table']='Занесення даних в таблиці';
+	$lang['settings_save']='Збереження налаштувань';
+	$lang['create_administrator']='Створення користувача з правами адміністратора';
+
+	$lang['step1_title']='Створення тек';
+	$lang['step2_title']='Створення таблиць';
+	$lang['step3_title']='Занесення даних в таблиці';
+	$lang['step4_title']='Встановлення налаштувань';
+	$lang['step5_title']='Запис налаштувань';
+	$lang['step6_title']='Створення користувача з правами адміністратора';
+	$lang['step7_title']='Створення користувача з правами адміністратора';
+	$lang['finish_title']='Систему встановлено!';
+
+	$lang['p_blocks']['main_menu']='Головне меню';
+	$lang['p_blocks']['admin_menu']='Меню адміністратора';
+	$lang['p_blocks']['login']='Вхід';
+
+	$lang['p_welcome_page']['title']='Вітаємо';
+	$lang['p_welcome_page']['text']='Вітаємо Вас в системі SiMan CMS. <br /><br /> Ця сторінка буде виводитись по замовчуванню.<br /><br />Для того, щоб змінити її вміст, <a href="index.php?m=account">увійдіть</a> в систему як адміністратор та натисніть на посилання внизу або <a href="index.php?m=content&d=edit&cid=1">сюди</a>.<br /><br />Для детального ознайомлення з системою ласкаво просимо на офіційний сайт <a href="http://siman.org.ua">siman.org.ua</a><br /><br /><strong>Приємної та плідної роботи!</strong>';
+
+	$lang['p_menu']['main_menu']='Меню';
+	$lang['p_menu']['admin_menu']='Адміністрування';
+
+	$lang['p_mlines']['main']='Головна';
+	$lang['p_mlines']['news']='Новини';
+	$lang['p_mlines']['adm_content']='Тексти';
+	$lang['p_mlines']['adm_news']='Новини';
+	$lang['p_mlines']['adm_new_content']='Новий текст';
+	$lang['p_mlines']['adm_new_news']='Додати новину';
+	$lang['p_mlines']['modules_management']='Керування модулями';
+	$lang['p_mlines']['control_panel']='Панель керування';
+
+	$lang['p_modules']['content']='Тексти';
+	$lang['p_modules']['news']='Новини';
+	$lang['p_modules']['menu']='Меню';
+	$lang['p_modules']['download']='Завантаження';
+	$lang['p_modules']['search']='Пошук';
+
+	$inst['p_news']['firs_news']='SiMan CMS встановлено!';
+	$inst['p_news']['firs_news_text']='Це приклад новини. Зайдіть під адміністративник обліковим записом щоб змінити або видалити її.';
+
+	$lang['settings_resource_title']='Назва сайту';
+	$lang['settings_resource_url']='Адреса сайту';
+	$lang['settings_logo_text']='Девіз сайту';
+	$lang['settings_copyright_text']='Текст копірайту';
+	$lang['settings_default_language']='Мова сайту';
+	$lang['settings_default_theme']='Тема сайту';
+
+	$lang['siman_powered_site']='Цей сайт використовує SiMan CMS';
+	$lang['use_siman']='Використовуйте SiMan CMS';
+	$lang['your_company']='Ваша компанія';
+	$lang['content_news']='В основному новини';
+	$lang['content_texts']='В основному тексти';
+
+	$lang['addadm']['login_str']='Логін';
+	$lang['addadm']['password']='Пароль';
+	$lang['addadm']['email']='Електронна пошта';
+	$lang['addadm']['message_wrong_login']='Ви ввели невірне ім\'я користувача чи пароль.';
+	$lang['addadm']['secret_question']='Запитання (якщо забули пароль)';
+	$lang['addadm']['secret_answer_question']='Відповідь на запитання';
+	$lang['addadm']['message_set_all_fields']='Одне з обов\'язкових полів не заповнене';
+	$lang['addadm']['message_bad_email']='Неправильна адреса електронної пошти';
+	$lang['addadm']['add_settings_admin_email']='Встановлення е-пошти адміністратора';
+
+	$lang['finished']['gratulations']='<strong>Поздоровляємо!</strong><br /><br />Ви успішно встановили SiMan CMS!';
+	$lang['finished']['dont_forget_erase']='Перед початком роботи з системою обов\'язково видаліть теку <strong>install</strong>.';
+	$lang['finished']['apserver_message']='Документацію, додаткові модулі, оновлення для SiMan CMS Ви можете знайти на наступних ресурсах';
+	$lang['finished']['official_site']='Офіційний сайт';
+	$lang['finished']['developers_portal']='Портал розробників';
+	$lang['finished']['begin_work']='Перейти до роботи з системою';
+
+?>
