a21178718a93e0774caaa624a1ebfc8154f12b21
Basil McCormac <basilmccormac@aol.com>
2014-04-15 13:51:12 +0300

    code improvement

--- includes/userinfo.php
+++ includes/userinfo.php
@@ -1,45 +1,41 @@
 <?php
 
-//------------------------------------------------------------------------------
-//|                                                                            |
-//|            Content Management System SiMan CMS                             |
-//|                http://simancms.org                                  |
-//|                                                                            |
-//------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
+	//|                                                                            |
+	//|            Content Management System SiMan CMS                             |
+	//|                                                                            |
+	//------------------------------------------------------------------------------
 
-//==============================================================================
-//#ver 1.6.4
-//==============================================================================
+	//==============================================================================
+	//#ver 1.6.6
+	//#revision 2014-04-15
+	//==============================================================================
 
-$userinfo['sid']=session_id();
+	$userinfo['sid'] = session_id();
 
-if (empty($_sessionvars['userinfo_id']))
-	{
-		$userinfo['id']='';
-		$userinfo['login']='';
-		$userinfo['email']='';
-		$userinfo['session']='';
-		$userinfo['level']=0;
-		$userinfo['groups']='';
-		$userinfo['info']=Array();
-	}
-else
-	{
-		$userinfo['id']=$_sessionvars['userinfo_id'];
-		$userinfo['login']=$_sessionvars['userinfo_login'];
-		$userinfo['email']=$_sessionvars['userinfo_email'];
-		$userinfo['level']=$_sessionvars['userinfo_level'];
-		$userinfo['session']=$userinfo['sid'];
-		$userinfo['groups']=$_sessionvars['userinfo_groups'];
-		$userinfo['info']=unserialize($_sessionvars['userinfo_allinfo']);
-	}
+	if (empty($_sessionvars['userinfo_id']))
+		{
+			$userinfo = Array(
+				'level' => 0,
+				'info' => Array()
+			);
+		}
+	else
+		{
+			$userinfo['id'] = $_sessionvars['userinfo_id'];
+			$userinfo['login'] = $_sessionvars['userinfo_login'];
+			$userinfo['email'] = $_sessionvars['userinfo_email'];
+			$userinfo['level'] = $_sessionvars['userinfo_level'];
+			$userinfo['session'] = $userinfo['sid'];
+			$userinfo['groups'] = $_sessionvars['userinfo_groups'];
+			$userinfo['info'] = unserialize($_sessionvars['userinfo_allinfo']);
+		}
 
-if ($userinfo['id']==1)
-	{
-		$userinfo['level']=3;
-		$userinfo['can_upload_image']=1;
-	}
+	if (intval($userinfo['id']) == 1)
+		{
+			$userinfo['level'] = 3;
+		}
 
-$smarty->assign('userinfo', $userinfo);
+	$smarty->assign('userinfo', $userinfo);
 
 ?>
