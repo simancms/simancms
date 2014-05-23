dddc1af8e953357fa274a2f109f7573e2353ce96
Basil McCormac <basilmccormac@aol.com>
2014-05-23 12:30:18 +0300

    adminbuttons rebuilding

--- includes/adminbuttons.php
+++ includes/adminbuttons.php
@@ -9,18 +9,26 @@
 if (!defined("adminbuttons_DEFINED"))
 	{
 		sm_add_cssfile('common_adminbuttons.css');
+		sm_add_jsfile('common_adminbuttons.js');
 		
 		class TButtons
 			{
 				var $bar;
+				private $currentbuttonname;
 				function TButtons($buttonbar_title='')
 					{
 						$this->bar['buttonbar_title']=$buttonbar_title;
+					}
+				function Button($title, $url='')
+					{
+						$this->AddButton('', $title, $url, 'button');
+						return $this;
 					}
 				function AddButton($name, $title, $url='', $type='button', $style='', $messagebox_message='', $javascript='')
 					{
 						if (empty($name))
 							$name=md5(rand(1, 999999));
+						$this->currentbuttonname=$name;
 						$this->bar['buttons'][$name]['name']=$name;
 						$this->bar['buttons'][$name]['caption']=$title;
 						$this->bar['buttons'][$name]['url']=$url;
@@ -28,45 +36,127 @@ if (!defined("adminbuttons_DEFINED"))
 						$this->bar['buttons'][$name]['javascript']=$javascript;
 						$this->bar['buttons'][$name]['style']=$style;
 						$this->bar['buttons'][$name]['message']=addslashes($messagebox_message);
+						return $this;
 					}
 				function AddSeparator($name, $title=' | ', $style='')
 					{
+						if (empty($name))
+							$name=md5(rand(1, 999999));
+						$this->currentbuttonname=$name;
 						$this->bar['buttons'][$name]['name']=$name;
 						$this->bar['buttons'][$name]['caption']=$title;
 						$this->bar['buttons'][$name]['style']=$style;
 						$this->bar['buttons'][$name]['type']='separator';
+						return $this;
 					}
 				function AddToggle($name, $title, $toggle_id, $style='')
 					{
 						$javascript="tmp=document.getElementById('".$toggle_id."');tmp.style.display=(tmp.style.display=='none')?'':'none';";
 						$this->AddButton($name, $title, '', 'button', $style, '', $javascript);
+						return $this;
+					}
+				function MessageBox($title, $url, $messagebox_message)
+					{
+						$this->AddMessageBox('', $title, $url, $messagebox_message);
+						return $this;
 					}
 				function AddMessageBox($name, $title, $url, $messagebox_message, $style='')
 					{
 						$this->AddButton($name, $title, $url, 'messagebox', $style, $messagebox_message);
+						return $this;
 					}
-				function Bold($name)
+				function Bold($buttonname=NULL)
 					{
-						$this->bar['buttons'][$name]['bold']=1;
+						if ($buttonname==NULL)
+							$buttonname=$this->currentbuttonname;
+						$this->bar['buttons'][$buttonname]['bold']=true;
+						return $this;
 					}
-				function Width($name, $width)
+				function Width($buttonname, $width)
 					{
-						$this->bar['buttons'][$name]['width']=$width;
+						if ($buttonname==NULL)
+							$buttonname=$this->currentbuttonname;
+						$this->bar['buttons'][$buttonname]['width']=$width;
+						return $this;
 					}
-				function Height($name, $height)
+				function SetWidth($width)
 					{
-						$this->bar['buttons'][$name]['height']=$height;
+						$this->Width(NULL, $width);
+						return $this;
+					}
+				function Height($buttonname, $height)
+					{
+						if ($buttonname==NULL)
+							$buttonname=$this->currentbuttonname;
+						$this->bar['buttons'][$buttonname]['height']=$height;
+						return $this;
+					}
+				function SetHeight($height)
+					{
+						$this->Height(NULL, $height);
+						return $this;
 					}
 				function Style($name, $style)
 					{
 						$this->bar['buttons'][$name]['style']=$style;
+						return $this;
 					}
 				function AssignImage($name, $imagename)
 					{
 						$this->bar['buttons'][$name]['image']=$imagename;
+						return $this;
+					}
+				function AddClassnameGlobal($classname)
+					{
+						$this->bar['class']=(empty($this->bar['class'])?'':' ').$classname;
+						return $this;
+					}
+				function AddClassname($classname, $buttonname=NULL)
+					{
+						if ($buttonname==NULL)
+							$buttonname=$this->currentbuttonname;
+						$this->bar['buttons'][$buttonname]['class']=(empty($this->bar['buttons'][$buttonname]['class'])?'':' ').$classname;
+						return $this;
+					}
+				function SetStyleGlobal($style)
+					{
+						$this->bar['style']=$style;
+						return $this;
 					}
 				function Output()
 					{
+						foreach ($this->bar['buttons'] as $buttonname=>&$buttonparams)
+							{
+								if ($buttonparams['type']=='separator')
+									{
+										$buttonparams['htmlelement']='span';
+										$buttonparams['class'].=(empty($buttonparams['class'])?'':' ').'ab-separator';
+									}
+								else
+									{
+										$buttonparams['htmlelement']='button';
+										$buttonparams['class'].=(empty($buttonparams['class'])?'':' ').'ab-button';
+									}
+								if ($buttonparams['bold'])
+									$buttonparams['style'].=(empty($buttonparams['style'])?'':';').'font-weight:bold;';
+								if (!empty($buttonparams['width']))
+									$buttonparams['style'].=(empty($buttonparams['style'])?'':';').'width:'.$buttonparams['width'].';';
+								if (!empty($buttonparams['height']))
+									$buttonparams['style'].=(empty($buttonparams['style'])?'':';').'height:'.$buttonparams['height'].';';
+								if (!empty($buttonparams['url']) || !empty($buttonparams['javascript']))
+									{
+										if ($buttonparams['type']=='messagebox')
+											$buttonparams['onclick']="button_msgbox('".$buttonparams['url']."', '".jsescape($buttonparams['message'])."');";
+										elseif (!empty($buttonparams['javascript']))
+											$buttonparams['onclick']=$buttonparams['javascript'];
+										else
+											$buttonparams['onclick']="location.href='".$buttonparams['url']."'";
+									}
+								$buttonparams['html']=$buttonparams['caption'];
+								$buttonparams['attrs']['style']=$buttonparams['style'];
+								$buttonparams['attrs']['onclick']=$buttonparams['onclick'];
+								$buttonparams['attrs']['class']=$buttonparams['class'];
+							}
 						$this->bar['count']=count($this->bar['buttons']);
 						return $this->bar;
 					}
--- themes/default/common_adminbuttons.tpl
+++ themes/default/common_adminbuttons.tpl
@@ -1,21 +1,5 @@
-<script type="text/javascript">
-{literal}
-function  button_msgbox(url, message)
-	{
-		if (confirm(message))
-			{
-				setTimeout(function() { document.location.href = url; }, 30);
-			}
-	}
-{/literal}
-</script>
-
-<div class="adminbuttons">{if $bar.buttonbar_title neq ""}<span class="buttonbar_title">{$bar.buttonbar_title}</span>{/if}
+<div class="adminbuttons{if $bar.class neq ""} {$bar.class}{/if}"{if $bar.style neq ""} style="{$bar.style}"{/if}>{if $bar.buttonbar_title neq ""}<span class="buttonbar_title">{$bar.buttonbar_title}</span>{/if}
 {foreach name=adminbuttons_index from=$bar.buttons item=button key=button_name}
-{if $button.type eq "separator"}
-	<span{if $button.style neq ""} style="{$button.style}"{/if}>{if $button.bold eq 1}<b>{/if}{if $button.image neq ""}<img src="{$button.image}" />{/if}{$button.caption}{if $button.bold eq 1}</b>{/if}</span>
-{else}
-	<button{if $button.url neq "" or $button.javascript neq ""} onclick="{if $button.type eq "messagebox"}button_msgbox('{$button.url}', '{$button.message}'){elseif $button.javascript neq ""}{$button.javascript}{else}location.href='{$button.url}'{/if}"{/if}{if $button.style neq "" or $button.width neq "" or $button.height neq ""} style="{$button.style}{if $button.width neq ""}width:{$button.width};{/if}{if $button.height neq ""}height:{$button.height};{/if}"{/if}>{if $button.bold eq 1}<b>{/if}{if $button.image neq ""}<img src="{$button.image}" />{/if}{$button.caption}{if $button.bold eq 1}</b>{/if}</button>
-{/if}
-{/foreach}	
+	<{$button.htmlelement}{foreach name=adminbuttonsattrs_index from=$button.attrs item=abattr key=abattr_name}{if $abattr neq ""} {$abattr_name}="{$abattr}"{/if}{/foreach}>{if $button.image neq ""}<img src="{$button.image}" />{/if}{$button.html}</{$button.htmlelement}>
+{/foreach}
 </div>
