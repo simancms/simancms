1629da1f58955b765d2d4cd50cce847f23c1b748
Joe <joe@bigmir.net>
2014-07-16 22:49:19 +0300

    code improvement

---
+++ includes/core/init.php
@@ -0,0 +1,89 @@
+<?php
+
+	//------------------------------------------------------------------------------
+	//|            Content Management System SiMan CMS                             |
+	//|                http://www.simancms.org                                     |
+	//------------------------------------------------------------------------------
+
+	//==============================================================================
+	//#ver 1.6.7
+	//#revision 2014-07-16
+	//==============================================================================
+
+	if (!$sm['disable_session'])
+		session_start();
+
+	$_getvars = $_GET;
+	$_postvars = $_POST;
+	$_cookievars = $_COOKIE;
+	$_servervars = $_SERVER;
+	$_uplfilevars = $_FILES;
+	if (!$sm['disable_session'])
+		{
+			if (is_array($_SESSION))
+				while (list($key, $val) = each($_SESSION))
+					{
+						if (strcmp(substr($key, 0, strlen($session_prefix)), $session_prefix) == 0)
+							{
+								$key = substr($key, strlen($session_prefix));
+								$_sessionvars[$key] = $val;
+							}
+					}
+		}
+
+	$special['main_tpl'] = 'index';
+	$special['page_url'] = 'index.php';
+	if (!empty($_servervars['QUERY_STRING']))
+		$special['page_url'] .= '?'.$_servervars['QUERY_STRING'];
+	$singleWindow = 0;
+
+	$special['printmode'] = 'off';
+	if (!empty($_getvars['printmode']))
+		{
+			if ($_getvars['printmode'] == 'on' || $_getvars['printmode'] == 1)
+				{
+					$special['printmode'] = 'on';
+					$special['main_tpl'] = 'indexprint';
+				}
+		}
+	if (!empty($_getvars['ajax']))
+		{
+			if ($_getvars['ajax'] == 1 || $_getvars['ajax'] == 'on')
+				{
+					$special['ajax'] = 1;
+					$special['main_tpl'] = 'simpleout';
+					$singleWindow = 1;
+				}
+		}
+	if ($_getvars['theonepage'] == 1 || $_getvars['theonepage'] == 'on')
+		{
+			$special['main_tpl'] = 'theonepage';
+			$special['no_blocks'] = true;
+			$special['no_borders_main_block'] = true;
+		}
+	if (!empty($_getvars['chngdsrc']))
+		{
+			if (is_numeric($_getvars['chngdsrc']))
+				{
+					if (!empty($_settings['allowed_db_prefixes'][$_getvars['chngdsrc']]))
+						$_sessionvars['overwritedbprefix'] = $_settings['allowed_db_prefixes'][$_getvars['chngdsrc']];
+				}
+		}
+	if (!empty($_sessionvars['overwritedbprefix']))
+		{
+			if ($tableusersprefix == $tableprefix)
+				$tableusersprefix = $_sessionvars['overwritedbprefix'];
+			$tableprefix = $_sessionvars['overwritedbprefix'];
+		}
+
+	$sm['g'] =& $_getvars;
+	$sm['p'] =& $_postvars;
+	$sm['server'] =& $_servervars;
+	$sm['cookies'] =& $_cookievars;
+	$sm['files'] =& $_uplfilevars;
+	$sm['session'] =& $_sessionvars;
+	$sm['s'] =& $special;
+	$sm['t'] =& $tableprefix;
+	$sm['tu'] =& $tableusersprefix;
+
+?>
--- index.php
+++ index.php
@@ -21,87 +21,16 @@
 	$special['time']['generation_begin'] = microtime();
 	$special['time']['generation_begin'] = explode(' ', $special['time']['generation_begin']);
 	$special['time']['generation_begin'] = $special['time']['generation_begin'][0] + $special['time']['generation_begin'][1];
-
-	session_start();
-
 	require_once("includes/dbsettings.php");
 	require_once("includes/dbengine".$serverDB.".php");
 	require_once("includes/dbelite.php");
 	require_once("includes/simplyquery.php");
-
-	$_getvars = $_GET;
-	$_postvars = $_POST;
-	$_cookievars = $_COOKIE;
-	$_servervars = $_SERVER;
-	$_uplfilevars = $_FILES;
-	if (is_array($_SESSION))
-		while (list($key, $val) = each($_SESSION))
-			{
-				if (strcmp(substr($key, 0, strlen($session_prefix)), $session_prefix) == 0)
-					{
-						$key = substr($key, strlen($session_prefix));
-						$_sessionvars[$key] = $val;
-					}
-			}
-
+	if (file_exists("includes/core/init_usr.php"))
+		require_once("includes/core/init_usr.php");
+	require_once("includes/core/init.php");
 	require_once("includes/functions.php");
 	require_once("includes/smcore.php");
 	require_once('Smarty/libs/Smarty.class.php');
-
-	$special['main_tpl'] = 'index';
-	$special['page_url'] = 'index.php';
-	if (!empty($_servervars['QUERY_STRING']))
-		$special['page_url'] .= '?'.$_servervars['QUERY_STRING'];
-	$singleWindow = 0;
-
-	$special['printmode'] = 'off';
-	if (!empty($_getvars['printmode']))
-		{
-			if ($_getvars['printmode'] == 'on' || $_getvars['printmode'] == 1)
-				{
-					$special['printmode'] = 'on';
-					$special['main_tpl'] = 'indexprint';
-				}
-		}
-	if (!empty($_getvars['ajax']))
-		{
-			if ($_getvars['ajax'] == 1 || $_getvars['ajax'] == 'on')
-				{
-					$special['ajax'] = 1;
-					$special['main_tpl'] = 'simpleout';
-					$singleWindow = 1;
-				}
-		}
-	if ($_getvars['theonepage'] == 1 || $_getvars['theonepage'] == 'on')
-		{
-			$special['main_tpl'] = 'theonepage';
-			$special['no_blocks'] = true;
-			$special['no_borders_main_block'] = true;
-		}
-	if (!empty($_getvars['chngdsrc']))
-		{
-			if (is_numeric($_getvars['chngdsrc']))
-				{
-					if (!empty($_settings['allowed_db_prefixes'][$_getvars['chngdsrc']]))
-						$_sessionvars['overwritedbprefix'] = $_settings['allowed_db_prefixes'][$_getvars['chngdsrc']];
-				}
-		}
-	if (!empty($_sessionvars['overwritedbprefix']))
-		{
-			if ($tableusersprefix == $tableprefix)
-				$tableusersprefix = $_sessionvars['overwritedbprefix'];
-			$tableprefix = $_sessionvars['overwritedbprefix'];
-		}
-
-	$sm['g'] =& $_getvars;
-	$sm['p'] =& $_postvars;
-	$sm['server'] =& $_servervars;
-	$sm['cookies'] =& $_cookievars;
-	$sm['files'] =& $_uplfilevars;
-	$sm['session'] =& $_sessionvars;
-	$sm['s'] =& $special;
-	$sm['t'] =& $tableprefix;
-	$sm['tu'] =& $tableusersprefix;
 
 	if (!isset($lnkDB))
 		$lnkDB = @database_connect($hostNameDB, $userNameDB, $userPasswordDB, $nameDB);
