b163852ef15c9f00cd84d9a9752281c58ec985e1
Basil McCormac <basilmccormac@aol.com>
2015-07-15 15:50:02 +0300

    core update + new events

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 	//==============================================================================
 	//#ver 1.6.9
-	//#revision 2015-05-07
+	//#revision 2015-07-15
 	//==============================================================================
 
 	if (!in_array(php_sapi_name(), Array('cli', 'cgi-fcgi')) && @get_magic_quotes_gpc() == 1)
@@ -229,8 +229,10 @@
 					if ($special['no_borders_main_block'])
 						$modules[$modules_index]['borders_off'] = 1;
 					$modules[$modules_index]['current_module'] = $module;
-					if ($module <> '404')
+					sm_event('beforemainblock');
+					if ($module != '404')
 						include('modules/'.$module.'.php');
+					sm_event('aftermainblock');
 					if (empty($modules[$modules_index]['module']))
 						{
 							$modules[$modules_index]['module'] = '404';
@@ -250,135 +252,9 @@
 
 					if (!$special['no_blocks'])
 						{
-							//Static blocks out
-							$sql = "SELECT * FROM ".$tableprefix."blocks ORDER BY position_block ASC";
-							$pnlresult = database_db_query($nameDB, $sql, $lnkDB);
-							while ($pnlrow = database_fetch_object($pnlresult))
-								{
-									$dont_show_high_priority = 0;
-									if ((intval($userinfo['level']) < intval($pnlrow->level) && $pnlrow->thislevelonly == 0) && !compare_groups($userinfo['groups'], $pnlrow->groups_view))
-										$dont_show_high_priority = 1;
-									elseif ((intval($userinfo['level']) > intval($pnlrow->level) && $pnlrow->thislevelonly == -1) && !compare_groups($userinfo['groups'], $pnlrow->groups_view))
-										$dont_show_high_priority = 1;
-									elseif (intval($userinfo['level']) != intval($pnlrow->level) && $pnlrow->thislevelonly == 1)
-										$dont_show_high_priority = 1;
-									elseif (!sm_is_index_page() && strcmp('#index#', $pnlrow->show_on_module) == 0 && $pnlrow->dont_show_modif != 1)
-										$dont_show_high_priority = 1;
-									if (!empty($pnlrow->showontheme) && $pnlrow->showontheme == sm_current_theme())
-										$dont_show_high_priority = 1;
-									if (!empty($pnlrow->showonlang) && $pnlrow->showonlang == $sm['s']['lang'])
-										$dont_show_high_priority = 1;
-									$show_panel = 1;
-									if (!empty($pnlrow->show_on_module))
-										{
-											$show_panel = 0;
-											if ((strcmp($pnlrow->show_on_module, $module) == 0 || (empty($pnlrow->show_on_doing) && empty($modules[0]["mode"]) || strcmp($pnlrow->show_on_doing, $modules[0]["mode"]) == 0)) || (sm_is_index_page() && strcmp('#index#', $pnlrow->show_on_module) == 0))
-												{
-													if ($pnlrow->show_on_ctg != 0)
-														{
-															if ($special['categories']['id'] == $pnlrow->show_on_ctg)
-																$show_panel = 1;
-														}
-													else
-														$show_panel = 1;
-												}
-										}
-									if ($pnlrow->dont_show_modif == 1)
-										$show_panel = abs($show_panel - 1);
-									if ($show_panel == 0 && !empty($pnlrow->show_on_viewids) && !empty($special['page']['viewid']))
-										{
-											$tmpviewidslist = nllistToArray($pnlrow->show_on_viewids);
-											for ($i = 0; $i < count($tmpviewidslist); $i++)
-												{
-													if ($tmpviewidslist[$i] == $special['page']['viewid'])
-														{
-															$show_panel = 1;
-															break;
-														}
-												}
-											unset($tmpviewidslist);
-										}
-									if ($show_panel == 1 && !empty($pnlrow->show_on_device))
-										{
-											if ($pnlrow->show_on_device == 'desktop' && !$special['deviceinfo']['is_desktop'])
-												$show_panel = 0;
-											elseif ($pnlrow->show_on_device == 'mobile' && !$special['deviceinfo']['is_mobile'])
-												$show_panel = 0;
-											elseif ($pnlrow->show_on_device == 'tablet' && !$special['deviceinfo']['is_tablet'])
-												$show_panel = 0;
-										}
-									if ($dont_show_high_priority == 1)
-										$show_panel = 0;
-									if ($show_panel == 1)
-										{
-											$modules_index++;
-											if ($pnlrow->panel_block == 'l')
-												{
-													$modules[$modules_index]["panel"] = "1";
-												}
-											elseif ($pnlrow->panel_block == 'r')
-												{
-													$modules[$modules_index]["panel"] = "2";
-												}
-											elseif (is_numeric($pnlrow->panel_block))
-												{
-													$modules[$modules_index]["panel"] = $pnlrow->panel_block;
-												}
-											else
-												{
-													$modules[$modules_index]["panel"] = "center";
-												}
-											$modules[$modules_index]["borders_off"] = $pnlrow->no_borders;
-											$modules[$modules_index]["bid"] = $pnlrow->showed_id;
-											$modules[$modules_index]["mode"] = $pnlrow->doing_block;
-											if ($_settings['blocks_use_image'] == 1)
-												{
-													if (file_exists('./files/img/block'.$pnlrow->id_block.'.jpg'))
-														{
-															$modules[$modules_index]['block_image'] = 'files/img/block'.$pnlrow->id_block.'.jpg';
-														}
-												}
-											$modules[$modules_index]['rewrite_title_to'] = $pnlrow->rewrite_title;
-											$m =& $modules[$modules_index];
-											$sm['m'] =& $modules[$modules_index];
-											if (empty($pnlrow->name_block) && !empty($pnlrow->text_block))
-												{
-													$m['title'] = $pnlrow->rewrite_title;
-													$m['mode'] = 'view';
-													$m['module'] = 'content';
-													$m['content'][0]['can_view'] = 1;
-													$m['content'][0]["text"] = $pnlrow->text_block;
-													sm_add_title_modifier($m['title']);
-													sm_add_content_modifier($m['content'][0]["text"]);
-												}
-											else
-												{
-													$modules[$modules_index]['current_module'] = $pnlrow->name_block;
-													include('modules/'.$pnlrow->name_block.'.php');
-												}
-										}
-									else
-										{
-											$modules_index++;
-											if ($pnlrow->panel_block == 'l')
-												{
-													$modules[$modules_index]["panel"] = "1";
-												}
-											elseif ($pnlrow->panel_block == 'r')
-												{
-													$modules[$modules_index]["panel"] = "2";
-												}
-											elseif (is_numeric($pnlrow->panel_block))
-												{
-													$modules[$modules_index]["panel"] = $pnlrow->panel_block;
-												}
-											else
-												{
-													$modules[$modules_index]["panel"] = "center";
-												}
-											$modules[$modules_index]["module"] = 'system_empty_block';
-										}
-								}
+							sm_event('beforestaticblocks');
+							include('includes/core/staticblocks.php');
+							sm_event('afterstaticblocks');
 						}
 
 					include('includes/core/postload.php');
