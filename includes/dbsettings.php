7229e0bfc1897d3eaeef99b784ebca9db38df32d
Joe <joe@bigmir.net>
2016-04-23 00:56:30 +0300

    siman 1.6.11 initialization

--- includes/dbsettings.php
+++ includes/dbsettings.php
@@ -14,7 +14,7 @@
 	$hostNameDB = 'localhost';
 
 	//Database name
-	$nameDB = 'siman_1610';
+	$nameDB = 'siman_1611';
 
 	//Database user name
 	$userNameDB = 'root';
@@ -34,7 +34,7 @@
 	$tableusersprefix = '';
 
 	//Session prefix. You need to change it for your site to prevent hacks
-	$session_prefix = 'needchange1610_';
+	$session_prefix = 'needchange1611_';
 
 	//Salt. You need to change it before installation to prevent hacks
 	// Do not change it after installation - all passwords will be lost
