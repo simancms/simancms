7bf356673d978a758b8a28d936d771c4518e050e
Joe <joe@bigmir.net>
2016-09-02 16:23:36 +0300

    YouTube replacer settings (enable-disable)

---
+++ lang/modules/en_youtubereplacer.php
@@ -0,0 +1,4 @@
+<?php
+
+	$lang['module_youtubereplacer']['module_youtubereplacer'] = 'YouTube Replacer';
+	$lang['module_youtubereplacer']['replacer'] = 'Time';
--- modules/youtubereplacer.php
+++ modules/youtubereplacer.php
@@ -8,7 +8,7 @@
 	Module Name: YouTube Replacer
 	Module URI: http://simancms.org/
 	Description: YouTube links replacement to embed code
-	Version: 2016-07-02
+	Version: 2016-09-02
 	Author: SiMan CMS Team
 	Author URI: http://simancms.org/
 	*/
@@ -18,19 +18,31 @@
 
 	if ($userinfo['level'] == 3)
 		{
+			sm_include_lang('youtubereplacer');
+			if (sm_action('savesettings'))
+				{
+					sm_add_settings('youtubereplaceron', intval($_postvars['youtubereplaceron']));
+					sm_notify($lang['messages']['settings_updated']);
+					sm_redirect('index.php?m='.sm_current_module().'&d=admin');
+				}
 			if (sm_action('admin'))
 				{
-					include_once('includes/admininterface.php');
-					include_once('includes/admintable.php');
+					sm_use('ui.interface');
+					sm_use('ui.form');
 					add_path_modules();
-					add_path('YouTube replacer', 'index.php?m=youtubereplacer&d=admin');
-					sm_title($lang['settings']);
+					add_path($lang['module_youtubereplacer']['module_youtubereplacer'], 'index.php?m=youtubereplacer&d=admin');
+					sm_title($lang['module_youtubereplacer']['module_youtubereplacer'].' - '.$lang['settings']);
 					$ui = new TInterface();
+					if (!empty($error_message))
+						$ui->NotificationError($error_message);
+					$f=new TForm('index.php?m='.sm_current_module().'&d=savesettings');
+					$f->AddSelectVL('youtubereplaceron', $lang['status'], Array(1, 0), Array($lang['common']['enabled'], $lang['common']['disabled']))->WithValue(sm_settings('youtubereplaceron'));
+					$ui->AddForm($f);
 					$ui->Output(true);
 				}
 			if (strcmp($m["mode"], 'install') == 0)
 				{
-					sm_register_module('youtubereplacer', 'YouTube replacer');
+					sm_register_module('youtubereplacer', $lang['module_youtubereplacer']['module_youtubereplacer']);
 					sm_add_settings('youtubereplaceron', 1);
 					sm_add_settings('youtubereplacerwidth', 480);
 					sm_add_settings('youtubereplacerheight', 385);
@@ -55,5 +67,3 @@
 					sm_redirect('index.php?m=admin&d=modules');
 				}
 		}
-
-?>
